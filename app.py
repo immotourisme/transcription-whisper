57260963ce70348be619ccb9c887c22d0640c2ee
immotourisme <dreadstock@gmail.com>
2025-03-14 09:00:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import ffmpeg
 # Assurer que FFmpeg est bien disponible
 os.environ["PATH"] += os.pathsep + "/usr/bin/"
 
-def transcribe_audio(file_path, model_size="base"):
+def transcribe_audio(file_path, model_size="tiny"):
     model = whisper.load_model(model_size)
     result = model.transcribe(file_path)
     return result["text"]
